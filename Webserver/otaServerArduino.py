a38655c9f4fd8c645136474fc9cb89960f745da8
techniccontroller <techniccontroller@gmail.com>
2020-04-18 17:18:55 +0200

    reduce fileserver to simple otaserver, cleanup code

--- Webserver/otaServerArduino.py
+++ Webserver/otaServerArduino.py
@@ -1,37 +1,13 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import subprocess
 from subprocess import call, PIPE
-import serial
 import time
 
 # path to the folder with the temporary stored file
 datapath ="data/"
-s = serial.Serial('/dev/arduino', 115200) # Namen ggf. anpassen
 
 
-class MyHandler(BaseHTTPRequestHandler):
-	def do_HEAD(client):
-		client.send_response(200)
-		client.send_header("Content-type","text/html")
-		client.end_headers()
-	def do_GET(client):
-		global datapath, s
-		
-		if client.path == "/":
-			# user gets small interface to upload files via drag and drop
-			client.send_response(200)
-			client.send_header("Content-type", "text/html")
-			client.end_headers()
-			client.wfile.write(load('index.html'))
-		elif client.path.startswith("/command"):
-			print('send command: ' + client.path[8:])
-			s.write(client.path[8:]+"\n")
-			response = s.readline()
-			print (response)
-			client.send_response(200)
-			client.end_headers()
-
-			
+class MyHandler(BaseHTTPRequestHandler):	
 
 	def do_POST(client):
 		if client.path == "/postArduinoCode":
@@ -83,32 +59,18 @@ def read_stderr(proc):
 			break
 	return res			
 			
-def load(file):
-	with open(file) as file:
-		return file.read()
 		
 def main():
 	try:
-		global s
-		# Route port 80 to port 8080
-		#call("sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080", shell = True)
-		#call("sudo iptables -t nat -I OUTPUT -p tcp -d 127.0.0.1 --dport 80 -j REDIRECT --to-port 8080", shell = True)
-		#call("sudo iptables -t nat -I OUTPUT -p tcp -d 192.168.137.1 --dport 80 -j REDIRECT --to-port 8080", shell = True)
-		# Start webserver to upload images
+		# Start webserver
 		server = HTTPServer(('',8080), MyHandler)
 		print('started httpserver on port 8080 ...')
 		print('stop with pressing Ctrl+C')
-		
-
-		
-		# s.open()
-		time.sleep(5) # der Arduino resettet nach einer Seriellen Verbindung, daher muss kurz gewartet werden
 		server.serve_forever()
  
 	except KeyboardInterrupt:
 		print('^C received, shutting down server')
 		server.socket.close()
-		s.close()
 
 if __name__ == '__main__':
 	main()
