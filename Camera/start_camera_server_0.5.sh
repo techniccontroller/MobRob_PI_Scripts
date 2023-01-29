699fc0b63276581227a76d02e6a696b6d7a40396
techniccontroller <techniccontroller@gmail.com>
2023-01-29 16:25:09 +0100

    add start and stop scripts

---
+++ Camera/start_camera_server_0.5.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+cd /home/pi/MobRob_PI_Scripts/; python3 /home/pi/MobRob_PI_Scripts/Camera/cameraTCPServer.py -s 0.5
+
---
+++ Camera/stop_camera_server.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+kill $(pgrep -f "python3 /home/pi/MobRob_PI_Scripts/Camera/cameraTCPServer.py")
